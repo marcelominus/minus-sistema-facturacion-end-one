223fa95f56e492e2065c848fd5c26f62dee02124
marcelominus <minusmaya.marcelopoma@gmail.com>
2021-04-06 17:55:01 -0400

    06042021 change

--- client/src/components/tools/SideBar.js
+++ client/src/components/tools/SideBar.js
@@ -1,9 +1,58 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+import { Layout, Menu } from "antd";
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
 
+// =====================================================
+// INICIO DE CLASE  */}
+// =====================================================
 const SideBar = () => {
+  //-------------------------------------------------------
+  //
+  const [collapsed, setCollapsed] = useState(false);
+  const onCollapse = () => {
+    setCollapsed(!collapsed);
+  };
+  // =====================================================
+  // INICIO DE COMPONENTE}
+  // =====================================================
   return (
     <Fragment>
-      <p>desde el SIDEBAR</p>
+      <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          defaultSelectedKeys={["1"]}
+          mode="inline"
+          collapsedWidth="20"
+        >
+          <Menu.Item key="1" icon={<PieChartOutlined />}>
+            Option 1
+          </Menu.Item>
+          <Menu.Item key="2" icon={<DesktopOutlined />}>
+            Option 2
+          </Menu.Item>
+          <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+            <Menu.Item key="3">Tom</Menu.Item>
+            <Menu.Item key="4">Bill</Menu.Item>
+            <Menu.Item key="5">Alex</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+            <Menu.Item key="6">Team 1</Menu.Item>
+            <Menu.Item key="8">Team 2</Menu.Item>
+          </SubMenu>
+          <Menu.Item key="9" icon={<FileOutlined />}>
+            Files
+          </Menu.Item>
+        </Menu>
+      </Sider>
     </Fragment>
   );
 };
--- client/src/page/admin/AdminStart.js
+++ client/src/page/admin/AdminStart.js
@@ -1,9 +1,90 @@
 import React, { Fragment } from "react";
+import { Layout, Menu, Breadcrumb, Modal, Button } from "antd";
+//Importamos framer motion
+import { motion } from "framer-motion";
+import {
+  pageTransitionStart,
+  pageVariantsStart,
+} from "../../resource/js/effects";
+//*******************************************************
+//
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+//*******************************************************
+//
+import SideBar from "../../components/tools/SideBar";
 
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+//*******************************************************
+//
+
+// =====================================================
+// INICIO DE CLASE  */}
+// =====================================================
 const AdminStart = () => {
+  // =====================================================
+  // INICIO DE COMPONENTE}
+  // =====================================================
   return (
     <Fragment>
-      <p>DESDE EL START</p>
+      <Layout style={{ minHeight: "100vh" }}>
+        {/* --------------------------------------------------  */}
+        <SideBar />
+        {/* --------------------------------------------------  */}
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }}>
+            <motion.div
+              initial="out"
+              animate="in"
+              exit="out"
+              variants={pageVariantsStart}
+              transition={pageTransitionStart}
+              className="container-padding"
+            >
+              <div style={{ fontSize: "1.5rem", color: "white" }}>Inicio</div>
+            </motion.div>
+          </Header>
+          <Content style={{ margin: "0 16px" }}>
+            {/* <div
+              className="site-layout-background"
+              style={{ padding: 24, minHeight: 360 }}
+            ></div> */}
+            <motion.div
+              initial="out"
+              animate="in"
+              exit="out"
+              variants={pageVariantsStart}
+              transition={pageTransitionStart}
+              className="container-padding"
+            >
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+                maxime ut beatae repudiandae! Quae et laudantium saepe est,
+                excepturi, blanditiis quos recusandae id, cum at sunt sit
+                corrupti omnis facilis.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Reprehenderit officiis delectus qui, aperiam in, enim modi
+                similique officia, expedita placeat consectetur! Quisquam
+                mollitia hic blanditiis soluta sed ipsum officiis iusto.cambios
+                d
+              </p>
+              <p>lorem</p>
+            </motion.div>
+          </Content>
+          {/* </motion.div> */}
+          <Footer style={{ textAlign: "center" }}>
+            Ant Design ©2018 Created by Ant UED
+          </Footer>
+        </Layout>
+      </Layout>
     </Fragment>
   );
 };
